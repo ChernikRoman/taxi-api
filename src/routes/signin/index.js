539a8c069f099dab8381166908fe215236392281
Roman Chernik <chernik.r@yandex.ru>
2023-03-02 23:58:48 +0300

    feat: добавил аутентификацию диспетчеров

--- src/routes/signin/index.js
+++ src/routes/signin/index.js
@@ -3,16 +3,27 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { dispatcherModel } = require('../../mongoose_models/desktopUsers.js');
 
-const { NODE_ENV, SECRET_KEY } = process.env;
+const { SECRET_KEY } = process.env;
 
 async function dispatcherSignIn(req, res, next) {
   const { login, password } = req.body;
 
-  const dispatcher = await dispatcherModel.findOne({ login });
+  const dispatcher = await dispatcherModel.findOne({ login }).select('+password').lean();
   try {
     bcrypt.compareSync(password, dispatcher.password);
-    const token = jwt.sign({ _id: dispatcher._id }, SECRET_KEY);
-    res.status(200).send({ token, message: 'Sign!' });
+
+    const token = jwt.sign(dispatcher, SECRET_KEY, { expiresIn: '7d' });
+    res.cookie('jwt', token, {
+      maxAge: 3600000 * 24 * 7,
+      httpOnly: true,
+      sameSite: true,
+    });
+    res.status(200).send({
+      firstName: dispatcher.firstName,
+      lastName: dispatcher.lastName,
+      login: dispatcher.login,
+      role: dispatcher.role,
+    });
   } catch (err) {
     next(new Error('Логин или пароль не совпадают'));
   }
