3e9f1b3dc5388d3272d6291f5f9f3d8f241626b3
Roman Chernik <chernik.r@yandex.ru>
2023-03-14 23:45:50 +0300

    feat: изменил схему driver

--- src/mongoose_models/driver.js
+++ src/mongoose_models/driver.js
@@ -1,40 +1,44 @@
 const mongoose = require('mongoose');
 
 const driverSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    minLength: 3,
-    maxLength: 15,
-    require: true,
+  info: {
+    firstName: {
+      type: String,
+      minLength: 3,
+      maxLength: 15,
+      require: true,
+    },
+    lastName: {
+      type: String,
+      minLength: 3,
+      maxLength: 15,
+      require: true,
+    },
+    role: {
+      type: String,
+      enum: ['driver'],
+      default: 'driver',
+      require: true,
+    },
+    car: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'car',
+    },
   },
-  lastName: {
-    type: String,
-    minLength: 3,
-    maxLength: 15,
-    require: true,
-  },
-  role: {
-    type: String,
-    enum: ['driver'],
-    default: 'driver',
-    require: true,
-  },
-  car: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'car',
-  },
-  login: {
-    type: String,
-    minLength: 12,
-    maxLength: 12,
-    require: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    minLength: 8,
-    require: true,
-    selected: true,
+  auth: {
+    login: {
+      type: String,
+      minLength: 12,
+      maxLength: 12,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      minLength: 8,
+      require: true,
+      selected: true,
+    },
   },
 });
 
